3424a83c3c4fa3f444c920b9c3fa5b59d2184507
venkataganesh48 <saiganesh8075@gmail.com>
2025-06-26 21:56:58 +0530

    Update ec2-action.sh

--- EC2-adv-level-1/ec2-action.sh
+++ EC2-adv-level-1/ec2-action.sh
@@ -3,9 +3,9 @@
 ACTION=$1  # notify or stop
 
 # Configuration
-INSTANCE_ID="i-07d9fc2449d1956f3"
-TOPIC_ARN="arn:aws:sns:ap-south-1:458799594567:ec2-alert-testing"
-REGION="ap-south-1"
+INSTANCE_ID="i-0e2c15149d8fafa79"
+TOPIC_ARN="arn:aws:sns:us-east-1:934977584611:ganesh-devops"
+REGION="us-east-1"
 
 if [ "$ACTION" == "notify" ]; then
   echo "Sending approval email..."
