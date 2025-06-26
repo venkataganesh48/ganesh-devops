fdc5cd0df0c5b705866fa8d95791197fb2749036
venkataganesh48 <saiganesh8075@gmail.com>
2025-06-26 21:41:04 +0530

    Delete Pipeline-scripts directory

--- Pipeline-scripts/stop_ec2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Configuration
-INSTANCE_ID="i-0123456789abcdef0"
-REGION="ap-south-1"
-
-echo "Stopping EC2 instance: $INSTANCE_ID in $REGION..."
-aws ec2 stop-instances --instance-ids $INSTANCE_ID --region $REGION
